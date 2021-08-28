10801bb972947d7e6402b8a28678c0e8e77cb8ff
giangmv <hoanggiang333889@gmail.com>
2021-08-28 17:18:53 +0700

    giang commit

--- src/generators.js
+++ src/generators.js
@@ -1,14 +1,13 @@
 import NuxtGenerator from "./generators/NuxtGenerator";
 import VueGenerator from "./generators/VueGenerator";
 import VuetifyGenerator from "./generators/VuetifyGenerator";
-import QuasarGenerator from "./generators/QuasarGenerator";
 
 function wrap(cl) {
   return ({ hydraPrefix, templateDirectory }) =>
     new cl({ hydraPrefix, templateDirectory });
 }
 
-export default function generators(generator = "react") {
+export default function generators(generator = "nuxt") {
   switch (generator) {
     case "nuxt":
       return wrap(NuxtGenerator);
@@ -16,7 +15,5 @@ export default function generators(generator = "react") {
       return wrap(VueGenerator);
     case "vuetify":
       return wrap(VuetifyGenerator);
-    case "quasar":
-      return wrap(QuasarGenerator);
   }
 }
